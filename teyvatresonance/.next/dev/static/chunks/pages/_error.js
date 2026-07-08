d7e7a08e365c4a5ae869bd4cfa9570a84a740dc6
Munib115 <munibhaseeb4@gmail.com>
2026-07-08 18:09:01 +0500

    chore: generate Turbopack development build assets for error page navigation

---
+++ teyvatresonance/.next/dev/static/chunks/pages/_error.js
@@ -0,0 +1,13 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/node_modules_next_dist_compiled_14ibvna._.js",
+  "static/chunks/node_modules_next_dist_shared_lib_12jmhie._.js",
+  "static/chunks/node_modules_next_dist_client_16lnmlo._.js",
+  "static/chunks/node_modules_next_dist_0zk-zb1._.js",
+  "static/chunks/1yot_next_dist_pages__error_1x3i9yc.js",
+  "static/chunks/[next]_entry_page-loader_ts_0x0528u._.js",
+  "static/chunks/node_modules_react-dom_0kla17-._.js",
+  "static/chunks/node_modules_10e2-xo._.js",
+  "static/chunks/[root-of-the-server]__1-bjo3j._.js",
+  "static/chunks/[project]_teyvatresonance_pages__error_0du2_q-._.js",
+  "static/chunks/turbopack-[project]_teyvatresonance_pages__error_15w8k4z._.js"
+])
